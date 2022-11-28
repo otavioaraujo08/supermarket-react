28e2fde8abac166f73ed72d2420efb477bb56a56
otavioaraujo08 <otavioaraujo490@gmail.com>
2022-11-28 15:19:16 -0300

    feat: starting project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Supermercado com react
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
