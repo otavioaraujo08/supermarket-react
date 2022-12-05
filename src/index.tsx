c21bdb044977a2b6e5894f1a2d41034900f9fc21
otavioaraujo08 <otavioaraujo490@gmail.com>
2022-12-05 12:23:50 -0300

    feat: change index logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Router from "./Routes/Routes";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Router} />
   </React.StrictMode>
 );
-
